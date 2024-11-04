9721a40b1bd7c6cb38a124b2b5192868c7ee464a
edilberto <ing.fera8@gmail.com>
2024-11-03 21:29:03 -0600

    Avanze 25.1

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10, 
-  queueLimit: 0
+  queueLimit: 0,
   connectTimeout: 10000, 
   acquireTimeout: 10000, 
 
